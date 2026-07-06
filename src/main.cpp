0564e67382ef509d2b46f43ee8f393433e513736
Kaisín Manning <manning.kaisin@gmail.com>
2026-07-06 15:10:50 +0100

    movement updated

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,53 @@
 #include <Arduino.h>
 
 const int LED_PIN = 1;
+const int IN1 = 4;
+const int IN2 = 5;
+const int IN3 = 6;
+const int IN4 = 7;
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   pinMode(LED_PIN, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
 }
 
-void loop() {
-  Serial.println("Hello, World!");
-  digitalWrite(LED_PIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_PIN, LOW);
-  delay(1000);
+void loop()
+{
+  // Rotate the Motor A clockwise
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  delay(2000);
+  // Motor A
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, HIGH);
+  delay(500);
+  // Rotate the Motor B clockwise
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  delay(2000);
+  // Motor B
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, HIGH);
+  delay(500);
+  // Rotates the Motor A counter-clockwise
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  delay(2000);
+  // Motor A
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, HIGH);
+  delay(500);
+  // Rotates the Motor B counter-clockwise
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  delay(2000);
+  // Motor B
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, HIGH);
+  delay(500);
 }
